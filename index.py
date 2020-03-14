2d7bc648799536892db4c8b70181e49daf1d71c0
Lumir-Ludvik <lumirludvik@email.cz>
2020-03-14 20:59:02 +0100

    Add list box

--- index.py
+++ index.py
@@ -4,14 +4,47 @@ from tkinter import font as tkFont
 from math import *
 
 class mainPage:
+
+    listDict = {
+        0: "Fruits & vegetables",
+        1: "Meat",
+        2: "Durable food",
+        3: "Drinks",
+        4: "Drugstore",
+        5: "Special offers"
+    }
+
     def __init__(self, root):
         self._root = root
         self._root.title("SimShop")
-        _createMenuBar()
+        self._createMenuBar()
+        self._createListBox()
 
+    def _onContactClick(self):
+        pass 
+
+    def _onAboutClick(self):
+        pass
+
+    def _onShoppingCartClick(self):
+        pass
+    
     def _createMenuBar(self):
-        self._menuBarFrame = Frame(self._root,)
-        self._menuBarFrame.pack(fill=X, expand=1, padx=4, pady=4)
+        self._menuBar = Menu(self._root)
+        self._menuBar.add_command(label="Contact", COMMAND=self._onContactClick())
+        self._menuBar.add_command(label="About", COMMAND=self._onAboutClick())
+        self._menuBar.add_command(label="ShoppingCart", COMMAND=self._onShoppingCartClick())
+        self._root.config(menu=self._menuBar)
+    
+    def _createListBox(self):
+        self._listBoxFrame = Frame(self._root)
+        self._listBox = Listbox(self._root)
+        for key, value in self.listDict.items():
+            self._listBox.insert(key, value)
+        self._listBox.pack(fill=Y, side=LEFT)
+
+
+
 
 def main():
     root = Tk()
